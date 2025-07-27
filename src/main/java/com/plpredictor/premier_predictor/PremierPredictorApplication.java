3215e084a61440e25139bd246c7626943ba8c399
ajamidar <jamidararnav@gmail.com>
2025-07-27 23:48:02 +0100

    Initial commit

---
+++ src/main/java/com/plpredictor/premier_predictor/PremierPredictorApplication.java
@@ -0,0 +1,13 @@
+package com.plpredictor.premier_predictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremierPredictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremierPredictorApplication.class, args);
+	}
+
+}
