5a3dcd81208b5a29fc15cc24d21fd9b1c96e732d
ajamidar <jamidararnav@gmail.com>
2025-07-31 14:53:30 +0100

    Create PlayerRepository.java

---
+++ src/main/java/com/plpredictor/premier_predictor/player/PlayerRepository.java
@@ -0,0 +1,14 @@
+package com.plpredictor.premier_predictor.player;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, String> {
+
+    void deleteByName(String playerName);
+    Optional<Player> findByName(String name);
+
+}
