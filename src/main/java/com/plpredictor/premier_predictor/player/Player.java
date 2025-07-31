868fab778961eb6546bc87c58198c050f3f803f5
ajamidar <jamidararnav@gmail.com>
2025-07-31 14:42:31 +0100

    Update Player.java

--- src/main/java/com/plpredictor/premier_predictor/player/Player.java
+++ src/main/java/com/plpredictor/premier_predictor/player/Player.java
@@ -1,4 +1,157 @@
 package com.plpredictor.premier_predictor.player;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="player_stats")
 public class Player {
+
+    @Id
+    @Column(name = "name", unique = true)
+    private String name;
+    private String nation;
+    private String pos;
+    private Integer age;
+    private Integer mp;
+    private Integer starts;
+    private Double min;
+    private Double gls;
+    private Double ast;
+    private Double pk;
+    private Double crdY;
+    private Double crdR;
+    private Double xag;
+    private String team;
+
+    public Player(String name, String nation, String pos, Integer age, Integer mp, Integer starts, Double min, Double gls, Double ast, Double pk, Double crdY, Double crdR, Double xag, String team) {
+        this.name = name;
+        this.nation = nation;
+        this.pos = pos;
+        this.age = age;
+        this.mp = mp;
+        this.starts = starts;
+        this.min = min;
+        this.gls = gls;
+        this.ast = ast;
+        this.pk = pk;
+        this.crdY = crdY;
+        this.crdR = crdR;
+        this.xag = xag;
+        this.team = team;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNation() {
+        return nation;
+    }
+
+    public String getPos() {
+        return pos;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Integer getMp() {
+        return mp;
+    }
+
+    public Integer getStarts() {
+        return starts;
+    }
+
+    public Double getMin() {
+        return min;
+    }
+
+    public Double getGls() {
+        return gls;
+    }
+
+    public Double getAst() {
+        return ast;
+    }
+
+    public Double getPk() {
+        return pk;
+    }
+
+    public Double getCrdY() {
+        return crdY;
+    }
+
+    public Double getCrdR() {
+        return crdR;
+    }
+
+    public Double getXag() {
+        return xag;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNation(String nation) {
+        this.nation = nation;
+    }
+
+    public void setPos(String pos) {
+        this.pos = pos;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setMp(Integer mp) {
+        this.mp = mp;
+    }
+
+    public void setStarts(Integer starts) {
+        this.starts = starts;
+    }
+
+    public void setMin(Double min) {
+        this.min = min;
+    }
+
+    public void setGls(Double gls) {
+        this.gls = gls;
+    }
+
+    public void setAst(Double ast) {
+        this.ast = ast;
+    }
+
+    public void setPk(Double pk) {
+        this.pk = pk;
+    }
+
+    public void setCrdY(Double crdY) {
+        this.crdY = crdY;
+    }
+
+    public void setCrdR(Double crdR) {
+        this.crdR = crdR;
+    }
+
+    public void setXag(Double xag) {
+        this.xag = xag;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
 }
