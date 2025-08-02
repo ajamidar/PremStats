19518b873d38ebe91078e1a9b3da3b8d80b8b86c
ajamidar <jamidararnav@gmail.com>
2025-08-02 10:11:53 +0100

    get functionalities implemented

--- src/main/java/com/plpredictor/premier_predictor/player/PlayerService.java
+++ src/main/java/com/plpredictor/premier_predictor/player/PlayerService.java
@@ -3,6 +3,9 @@ package com.plpredictor.premier_predictor.player;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Component
 public class PlayerService {
 
@@ -11,5 +14,40 @@ public class PlayerService {
     @Autowired
     public PlayerService(PlayerRepository playerRepository) {
         this.playerRepository = playerRepository;
+    }
+
+    public List<Player> getPlayers(){
+        return playerRepository.findAll();
+    }
+
+    public List<Player> getPlayersFromTeam(String teamName) {
+        return playerRepository.findAll().stream().
+                filter(player -> teamName.equals(player.getTeam()))
+                .collect(Collectors.toList());
+    }
+
+    public List<Player> getPlayersByName(String searchText) {
+        return playerRepository.findAll().stream().
+                filter(player -> player.getName().toLowerCase().contains(searchText.toLowerCase())).
+                collect(Collectors.toList());
+    }
+
+    public List<Player> getPlayersByPosition(String searchText) {
+        return playerRepository.findAll().stream().
+                filter(player -> player.getPos().toLowerCase().contains(searchText.toLowerCase()))
+                .collect(Collectors.toList());
+    }
+
+    public List<Player> getPlayersByNation(String searchText) {
+        return playerRepository.findAll().stream().
+                filter(player -> player.getNation().toLowerCase().contains(searchText.toLowerCase())).
+                collect(Collectors.toList());
+
+    }
+
+    public List<Player> getPlayersByTeamAndPosition(String team, String position) {
+        return playerRepository.findAll().stream().
+                filter(player -> team.equals(player.getTeam()) && position.equals(player.getPos())).
+                collect(Collectors.toList());
     }
 }
