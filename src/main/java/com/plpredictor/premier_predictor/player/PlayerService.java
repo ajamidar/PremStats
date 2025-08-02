3516ba89d34072ffe3aaf0d38c267afbefd8bee9
ajamidar <jamidararnav@gmail.com>
2025-08-02 10:59:17 +0100

    PlayerController implemented

--- src/main/java/com/plpredictor/premier_predictor/player/PlayerService.java
+++ src/main/java/com/plpredictor/premier_predictor/player/PlayerService.java
@@ -1,5 +1,6 @@
 package com.plpredictor.premier_predictor.player;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -74,6 +75,7 @@ public class PlayerService {
         return null;
     }
 
+    @Transactional
     public void deletePlayer(String playerName) {
         playerRepository.deleteByName(playerName);
     }
